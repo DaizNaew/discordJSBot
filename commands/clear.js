6d8e1720ac78f4b1bfb190f82df19d9968d21c48
DaizNaew <daiznaew@gmail.com>
2017-12-06 15:29:14 +0100

    Added clear command and fixed the issues listed in main

---
+++ commands/clear.js
@@ -0,0 +1,37 @@
+exports.run = (client, message, params) => {
+    const messagecount = parseInt(params.join(' '));
+    message.channel.fetchMessages({
+        limit:messagecount+1
+    })
+    .then(msg => {
+
+        message.channel.bulkDelete(msg);
+        message.channel.send(`Deleted: ${messagecount} messages.`)
+        .then(msg => {
+            setTimeout(function(){
+                msg.delete();
+            }, 5000);
+            
+        })
+        .catch(error => {
+            console.log('Could not find my own message anymore :( : \n' + error);
+        });
+    })
+    .catch(error => {
+        message.channel.send('Something went wrong in me belly. :( : \n '+ error);
+    });
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['clear','purge','Purge'],
+    permLevel: 2
+}
+
+exports.help = {
+    name: 'Clear',
+    description: 'Clears <n> messages in the current chat, informs the users, then deletes its own message',
+    usage: 'Clear <amount>  where amount is between 1 and 25 for now'
+}
+    
