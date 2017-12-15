b29847b28dbfbe6b69cbb61c8d0b4cbea06f15db
DaizNaew <daiznaew@gmail.com>
2017-12-15 08:40:41 +0100

    Roll Command :- minor bugfix and formatting

--- commands/roll.js
+++ commands/roll.js
@@ -3,27 +3,36 @@ var fp = require("lodash/fp");
 
 exports.run = (client, message, params) => {
 
-    let input = [] = params[0].split("d");
-    let results = [];
-    let numbers = "";
-    let numbersJoined;
+    message.channel.send("Rolling...")
+    .then(msg => {
 
-    for(i = 0; i < input[0]; i++ ) results[i] = _.random(1, input[1]);
+        let input = [] = params[0].split("d");
+        let results = [];
+        let numbers = "";
+        let debug;
+        let numbersJoined;
 
-    results.forEach(number => {
-        if(results.length === 1) numbers = number;
-        if(results.length > 1) numbers += number + " and ";
+        for(i = 0; i < input[0]; i++ ) results[i] = _.random(1, input[1]);
+    
+        results.forEach(number => {
+            if(results.length === 1){numbers = "**"+number+"**"; debug = 0;} 
+            if(results.length > 1) {numbers += "**"+number + "** and "; debug = 1;}
+        });
+        if(debug === 1) {
+            numbersJoined= numbers.split(" ");
+                numbersJoined.pop();numbersJoined.pop();
+                numbers = numbersJoined.join(" ");
+        }
+
+        if(!numbers) return msg.edit("Please enter a valid roll format");
+
+        msg.edit(`${message.author} I just rolled: ` 
+        + numbers
+        + "." );
+    })
+    .catch(error => {
+        message.channel.send('Something went wrong inside me. 😞 : \n '+ error);
     });
-
-    if(numbers.length > 1) {
-        numbersJoined= numbers.split(" ");
-            numbersJoined.pop();numbersJoined.pop();
-            numbers = numbersJoined.join(" ");
-    }
-    message.reply(`I just rolled: ` 
-    + numbers
-    + "." );
-
 }
 
 exports.conf = {
