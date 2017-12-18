98f579631f83236272480afefcb07317f25e00f9
DaizNaew <daiznaew@gmail.com>
2017-12-18 13:17:27 +0100

    Added twitch command to link to a default twitch channel + fixed a minor issue with default channels.

---
+++ commands/twitch.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const defTwitch = JSON.parse(fs.readFileSync("./storage/defaultTwitch.json", "utf8"));
+exports.run = (client, message) => {
+
+    message.channel.send('Fetching Twitch link..')
+    .then( msg => {
+        const twitchLink = defTwitch[message.guild.name];
+        msg.edit(`Come watch at: ${twitchLink.defaultTwitch}`);
+    })
+    .catch( error => {
+        message.channel.send('Something went wrong inside me. 😞 : \n '+ error);
+    });
+
+}
+
+exports.conf = {
+   enabled: true,
+   guildOnly: false,
+   aliases: ['Twitch', 'stream'],
+   permLevel: 0
+}
+
+exports.help = {
+   name: 'twitch',
+   description: 'It does something to twitch, inappropiate most likely',
+   usage: 'twitch'
+}
