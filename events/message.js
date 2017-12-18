fc6843f8813f83917cb66a3a645943a3be68b2fe
DaizNaew <daiznaew@gmail.com>
2017-12-18 13:51:48 +0100

    Actually fixed crashes killing the log

--- events/message.js
+++ events/message.js
@@ -6,12 +6,12 @@ module.exports = (message) => {
 
     const client = message.client;
     if (message.author.bot) return;
-    if (!message.content.startsWith(settings.prefix)) return;
+    if (!message.content.startsWith(settings.prefix)) return logging(message);
     const command = message.content.split(' ')[0].slice(settings.prefix.length);
     const params = message.content.split(' ').slice(1);
 
     //console.dir(message);
-
+    
     let cmd;
     if (client.commands.has(command)) {
         cmd = client.commands.get(command);
@@ -20,8 +20,9 @@ module.exports = (message) => {
     }
     if (cmd) {
         cmd.run(client, message, params);
+        logging(message);
     }
 
-    logging(message);
+    
 
 };
