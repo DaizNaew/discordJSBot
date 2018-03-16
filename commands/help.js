8b5d2b1a57f5b24622d38974980b01008c560554
DaizNaew <daiznaew@gmail.com>
2018-03-16 11:13:33 +0100

    Updated help to be more dynamic

--- commands/help.js
+++ commands/help.js
@@ -8,15 +8,17 @@ exports.run = (client, message, params) => {
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     message.channel.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(
         c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+    log(`Help command used by ${m.cyan.bold(message.author.tag)} in ${m.cyan.bold(message.channel.name)} on ${m.cyan.bold(message.guild.name)}`);
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
       let aliases = command.conf.aliases;
       message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}\naliases::${aliases}`, {code:'asciidoc'});
+      log(`Help command used by ${m.cyan.bold(message.author.tag)} to show help about ${m.cyan.bold(command.help.name)} in ${m.cyan.bold(message.channel.name)} on ${m.cyan.bold(message.guild.name)}`);
     }
   }
-  log(`Help command used by ${m.cyan.bold(message.author.tag)} in ${m.cyan.bold(message.channel.name)} on ${m.cyan.bold(message.guild.name)}`);
+  
 };
 
 exports.conf = {
