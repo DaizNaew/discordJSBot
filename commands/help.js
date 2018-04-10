fac5974e7b3ad2d73723ffbd86ed2214ed2d7544
DaizNaew <daiznaew@gmail.com>
2018-04-10 13:47:45 +0200

    Updated help to show changes in prefix

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,17 @@
 exports.run = (client, message, params, command_success, command_fail) => {
-    const settings = require('../config.json');
+    const   settings = require('../config.json'),
+            func = require('../func/propFunctions'),
+            serverSettings = func.readFromFileSync('./config/serverSettings.json');
+
     if (!params[0]) {
+
+        prefix = settings.prefix;
+        if(message.guild) prefix = serverSettings[message.guild.id]['configs'][0].prefix;
 
         const commandNames = Array.from(client.commands.keys());
         const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-        message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(
-        c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
+        message.author.send(`= Command List =\n\n[Use ${prefix}help <commandname> for details]\n\n${client.commands.map(
+        c => `${prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
         message.react(command_success);
         
     } else {
