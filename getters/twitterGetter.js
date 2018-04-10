620803391596e2c1c662ca53383088d8b172be16
DaizNaew <daiznaew@gmail.com>
2018-04-10 08:19:46 +0200

    Twitter getter now returns the tweet as promised. Also cleaned up all the old code. Still need to fix a couple issues and move some of it to a seperate module

--- getters/twitterGetter.js
+++ getters/twitterGetter.js
@@ -62,6 +62,7 @@ module.exports = {
                         `${tweet.user.profile_image_url_https}`,
                         `${tempText}`
                     ));
+                    return tweet;
                 });
             }));
         });
