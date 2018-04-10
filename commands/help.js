89d10f216df64b7639de5d8e1be5ab04340cc8ae
DaizNaew <daiznaew@gmail.com>
2018-04-10 09:19:25 +0200

    Help now messages the sender The help list is so very long now, Can't have it posted in channel

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = (client, message, params, command_success, command_fail) => {
     const commandNames = Array.from(client.commands.keys());
 
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(
+    message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(
     c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
     message.react(command_success);
   } else {
@@ -13,7 +13,7 @@ exports.run = (client, message, params, command_success, command_fail) => {
     if (client.commands.has(command)) {
       command = client.commands.get(command);
       let aliases = command.conf.aliases;
-      message.channel.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\nusable in guild only:: ${command.conf.guildOnly}\naliases:: ${aliases}`, {code:'asciidoc'});
+      message.author.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\nusable in guild only:: ${command.conf.guildOnly}\naliases:: ${aliases}`, {code:'asciidoc'});
       message.react(command_success);
     } else if(!client.commands.has(command)) {
       message.react(command_fail);
