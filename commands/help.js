eb7e1d978367deb88384556eed31a4d91400d2a9
DaizNaew <daiznaew@gmail.com>
2018-04-10 12:21:38 +0200

    Litteraly just changed indentation

--- commands/help.js
+++ commands/help.js
@@ -1,35 +1,35 @@
 exports.run = (client, message, params, command_success, command_fail) => {
-  const settings = require('../config.json');
-  if (!params[0]) {
+    const settings = require('../config.json');
+    if (!params[0]) {
 
-    const commandNames = Array.from(client.commands.keys());
-
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(
-    c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
-    message.react(command_success);
-  } else {
-    let command = params[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      let aliases = command.conf.aliases;
-      message.author.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\nusable in guild only:: ${command.conf.guildOnly}\naliases:: ${aliases}`, {code:'asciidoc'});
-      message.react(command_success);
-    } else if(!client.commands.has(command)) {
-      message.react(command_fail);
+        const commandNames = Array.from(client.commands.keys());
+        const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+        message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(
+        c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
+        message.react(command_success);
+        
+    } else {
+        let command = params[0];
+        if (client.commands.has(command)) {
+            command = client.commands.get(command);
+            let aliases = command.conf.aliases;
+            message.author.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\nusable in guild only:: ${command.conf.guildOnly}\naliases:: ${aliases}`, { code: 'asciidoc' });
+            message.react(command_success);
+        } else if (!client.commands.has(command)) {
+            message.react(command_fail);
+        }
     }
-  }
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'ineedanadult'],
-  permLevel: 0
+    enabled: true,
+    guildOnly: false,
+    aliases: ['h', 'halp', 'help', 'ineedanadult'],
+    permLevel: 0
 };
 
 exports.help = {
-  name: 'help',
-  description: 'Displays all the available commands',
-  usage: 'help <command>'
+    name: 'help',
+    description: 'Displays all the available commands',
+    usage: 'help <command>'
 };
