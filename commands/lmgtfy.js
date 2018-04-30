bf9cf6dd41c236bc063e22e797e97c8370a7a40c
DaizNaew <daiznaew@gmail.com>
2018-04-30 12:23:50 +0200

    lmgtfy command is now a proper response

--- commands/lmgtfy.js
+++ commands/lmgtfy.js
@@ -4,7 +4,8 @@ const m = require('chalk'),
 exports.run = (client, message, params, command_success, command_fail) => {
     message.channel.send('Googling...')
     .then(msg => {
-        const link = `http://lmgtfy.com/?q=${params}`;
+        let input = params.slice(0).join("+");
+        const link = `http://lmgtfy.com/?q=${input}`;
         msg.edit(link);
         message.react(command_success);
     })
