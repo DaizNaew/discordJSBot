f2ee45fbf99398a23b758feef6bfbbd7b57c966e
DaizNaew <daiznaew@gmail.com>
2018-05-07 14:47:58 +0200

    Disabled on react event

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,11 +1,11 @@
 const reqEvent = (event) => require(`../events/${event}`);
-const   RC = require('reaction-core'),
-        handler = new RC.Handler();
+//const   RC = require('reaction-core'),
+        //handler = new RC.Handler();
 
 module.exports = client => {
     client.on('ready', () => reqEvent('ready')(client));
     client.on('message', reqEvent('message'));
-    client.on('messageReactionAdd', (messageReaction, user) => handler.handle(messageReaction, user));
+    //client.on('messageReactionAdd', (messageReaction, user) => handler.handle(messageReaction, user));
     //client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
     //client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
 };
