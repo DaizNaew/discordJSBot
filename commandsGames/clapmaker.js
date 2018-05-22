e7ef7dae484d7cce4c1a7b8cebb8d290c0be310d
DaizNaew <daiznaew@gmail.com>
2018-05-22 13:28:53 +0200

    Added a clapmaker command

---
+++ commandsGames/clapmaker.js
@@ -0,0 +1,31 @@
+        //Local Files
+const   log = require('../enum/consoleLogging'),
+        func = require('../func/propFunctions'),
+        //NodeJS Modules
+        m = require('chalk');
+
+exports.run = (client, message, params, command_success, command_fail) => {
+    if(!params[0]) return message.channel.send('The cow does not want to be mute');
+    let input = params.slice(0).join(":clap:");
+    message.channel.send('Counting words...')
+    .then(msg => {
+        message.delete();
+        msg.edit(input)
+    })
+    .catch(err => {
+        log.error(err);
+    })
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'clapsay',
+    description: 'Outputs a say in an anoying fashion',
+    usage: 'clapsay <input>'
+}
