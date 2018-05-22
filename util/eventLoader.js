b302808cab3aa62ac6f79f5608e294f5a810c22d
DaizNaew <daiznaew@gmail.com>
2018-05-22 14:24:12 +0200

    Ported the reaction system from THICCBOT to DBot

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,11 +1,9 @@
 const reqEvent = (event) => require(`../events/${event}`);
-//const   RC = require('reaction-core'),
-        //handler = new RC.Handler();
 
 module.exports = client => {
     client.on('ready', () => reqEvent('ready')(client));
     client.on('message', reqEvent('message'));
-    //client.on('messageReactionAdd', (messageReaction, user) => handler.handle(messageReaction, user));
+    client.on('messageReactionAdd', (reaction, user) => reqEvent('reaction')(reaction, user, client));
     //client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
     //client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
 };
