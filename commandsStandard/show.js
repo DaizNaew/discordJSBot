7db3968cfd1e8e335c75f1855d49a6c08d0a5460
DaizNaew <daiznaew@gmail.com>
2018-06-26 09:02:15 +0200

    updated show command to reflect the changes done to userLogging

--- commandsStandard/show.js
+++ commandsStandard/show.js
@@ -3,7 +3,6 @@ const   log = require('../enum/consoleLogging'),
         embed = require('../model/embeds'),
         func = require('../func/propFunctions'),
         //NodeJS modules
-        _ = require('lodash'),
         m = require('chalk'),
         {Util} = require('discord.js');
         
@@ -11,8 +10,7 @@ const   log = require('../enum/consoleLogging'),
 exports.run = (client, message, params, command_success, command_fail) => {
     message.channel.send('Fetching...', {code: 'asciidoc'})
     .then(msg => {
-        clientLog = func.readFromFileSync('./storage/clientLog.json');
-        msg.edit(showUserLog(message, clientLog), {code: 'asciidoc'});
+        msg.edit(showUserLog(message), {code: 'asciidoc'});
         message.react(command_success);
     })
     .catch(error => {
@@ -36,13 +34,13 @@ exports.help = {
     usage: 'show <tag a user, like @them>  /Supply no user to check yo self '
 }
 
-    function showUserLog(message, clientLog){
+    function showUserLog(message){
         const guildName = message.guild.name;
         const mention =  message.mentions.members.first();
         const author = message.author;
         
         let userToShow;
-        let id;
+
         if(!mention) {
             id = author.id;
             userToShow = message.member;
@@ -50,8 +48,17 @@ exports.help = {
             id = mention.id;
             userToShow = mention;
         }
+
+        try {
+            clientLog = func.returnUserDate(userToShow);
+        } catch(err) {
+            clientLog = new Object;
+            clientLog.userTag = userToShow.user.tag;
+            clientLog.messages = 0;
+        }
+        
         let response,
-            game_name = 'None';
+            game_name = 'Nothing';
             
         if(userToShow.user.presence.game != null) game_name = userToShow.user.presence.game.name;
 
@@ -60,16 +67,16 @@ exports.help = {
         let created_date = new Date(userToShow.user.createdAt).toLocaleString('en-us', {  year: 'numeric',  day: 'numeric', month: 'long' });
         let joined_date = new Date(userToShow.joinedAt).toLocaleString('en-us', {  year: 'numeric',  day: 'numeric', month: 'long' });
 
-        if(clientLog[guildName][id]) {
+        if(clientLog) {
             let avatar = userToShow.user.avatarURL;
-            response = `I have a log on this person : ${clientLog[guildName][id].usertag}`;
+            response = `I have a log on this person : ${clientLog.userTag}`;
             message.author.send(
                 embed.RichEmbed(
                     null,
-                    clientLog[guildName][id].usertag,
+                    clientLog.userTag,
                     ['Server Name', Util.escapeMarkdown(guildName),false,
-                    'isBot: ',clientLog[guildName][id].clientisbot,true,
-                    'Amount of sent messages: ',clientLog[guildName][id].messagesSent,true,
+                    'isBot: ',userToShow.user.bot,true,
+                    'Amount of sent messages: ',clientLog.messages,true,
                     'Highest role on server: ',userToShow.highestRole.name,true,
                     'Currently playing: ', game_name,true, //delet this
                     'User created at: ', created_date,true,
