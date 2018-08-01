e66c2d064b5dc5cb824e9fc950fef3f475f4a523
DaizNaew <daiznaew@gmail.com>
2018-08-01 12:50:39 +0200

    Fixed help command

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -3,7 +3,7 @@ const reqEvent = (event) => require(`../events/${event}`);
 module.exports = (client, active) => {
     client.on('ready', () => reqEvent('ready')(client));
     client.on('message', (message) => reqEvent('message')(message, active));
-    //client.on('messageReactionAdd', (reaction, user) => reqEvent('reaction')(reaction, user, client));
+    client.on('messageReactionAdd', (reaction, user) => reqEvent('reaction')(reaction, user, client));
     client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
     //client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
 };
