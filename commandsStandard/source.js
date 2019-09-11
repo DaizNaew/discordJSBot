d9ab7493604723ab7d1b4e0f296718643599d42c
Daiz Naew <daiznaew@gmail.com>
2019-09-11 10:27:20 +0200

    Updated the source command to work again
    Made it so that the source command actually queries github for a specific repository, instead of trying to search for it inside a specific user.

--- commandsStandard/source.js
+++ commandsStandard/source.js
@@ -7,22 +7,26 @@ exports.run = (client, message, params, command_success, command_fail) => {
 
     message.channel.send("Fetching Sources...")
     .then(msg => {
-        require('../getters/githubGetter')(message,'users/DaizNaew/repos','sort=discordJSBot')
+        require('../getters/githubGetter')(message,'search/repositories','q=discordJSBot+in:name+daiznaew')
         .then(response => {
             msg.edit(
                 cEmbed.RichEmbed(
-                    [response.data[0].owner.login, `https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png`],
-                    `${response.data[0].name} on GitHub`,
-                    ['Language', `${response.data[0].language}`,true, `License type`, `${response.data[0].license.name}`, true],
+                    [response.data.items[0].owner.login, `https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png`],
+                    `${response.data.items[0].name} on GitHub`,
+                    ['Language', `${response.data.items[0].language}`,true, `License type`, `${response.data.items[0].license.name}`, true],
                     0x333333,
                     null,
-                    `${response.data[0].html_url}`,
+                    `${response.data.items[0].html_url}`,
                     null,
-                    `${response.data[0].owner.avatar_url}`,
-                    `${response.data[0].description}`
+                    `${response.data.items[0].owner.avatar_url}`,
+                    `${response.data.items[0].description}`
                 )
             );
             message.react(command_success);
+        }).catch(error => {
+            message.channel.send('Something went wrong inside me. 😞 : \n '+ error);
+            log.error(`Source command failed to execute [${error}]`);
+            message.react(command_fail);
         });
     })
     .catch(error => {
